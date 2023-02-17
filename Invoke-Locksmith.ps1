f73dde22374954a7d8a9f79d095584d0d2799263
Sam Erde <sam.erde@martinspoint.org>
2023-02-17 12:59:23 -0500

    Added Test-IsLocalAccountSession function

--- Invoke-Locksmith.ps1
+++ Invoke-Locksmith.ps1
@@ -135,6 +135,27 @@ function Test-IsADAdmin {
 }
 
 
+function Test-IsLocalAccountSession {
+    <#
+    .SYNOPSIS
+        Tests if the current session is running under a local user account or a domain account.
+    .DESCRIPTION
+        This function returns True if the current session is a local user or False if it is a domain user.
+    .EXAMPLE
+        Test-IsLocalAccountSession
+    .EXAMPLE
+        if ( (Test-IsLocalAccountSession) ) { Write-Host "You are running this script under a local account." -ForeGroundColor Yellow }
+    #>
+        [CmdletBinding()]
+    
+        $CurrentSID = [Security.Principal.WindowsIdentity]::GetCurrent().User.Value
+        $LocalSIDs = (Get-LocalUser).SID.Value
+        if ($CurrentSID -in $LocalSIDs) {
+            Return $true
+        }
+    }
+
+
 function Get-RestrictedAdminModeSetting {
     $Path = 'HKLM:SYSTEM\CurrentControlSet\Control\Lsa'
     try {
