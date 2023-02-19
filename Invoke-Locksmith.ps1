ad47fa8fae138072ff7b205e9d7213fca52ec9aa
Spencer Alessi <alessisb@gmail.com>
2023-02-18 22:02:10 -0500

    Add support for rsat install on desktop os

--- Invoke-Locksmith.ps1
+++ Invoke-Locksmith.ps1
@@ -636,10 +636,17 @@ if ($Credential) {
 
 # Check if ActiveDirectory PowerShell module is available, and attempt to install if not found
 if (-not(Get-Module -Name 'ActiveDirectory' -ListAvailable)) { 
-    # Attempt to install ActiveDirectory PowerShell module for Windows Server OSes, works with Windows Server 2012 R2 through Windows Server 2022
-    Install-WindowsFeature -Name RSAT-AD-PowerShell
-    #TODO: Check for Windows 10/11 OS (admin workstation, PAW) and install using Add-WindowsCapability cmdlet instead
+    $OS = (Get-CimInstance -ClassName Win32_OperatingSystem).ProductType
+    # 1 - workstation, 2 - domain controller, 3 - non-dc server
+    if ($OS -gt 1) {
+        # Attempt to install ActiveDirectory PowerShell module for Windows Server OSes, works with Windows Server 2012 R2 through Windows Server 2022
+        Install-WindowsFeature -Name RSAT-AD-PowerShell
+    } else {
+        # Attempt to install ActiveDirectory PowerShell module for Windows Desktop OSes
+        Add-WindowsCapability -Name Rsat.ActiveDirectory.DS-LDS.Tools~~~~0.0.1.0 -Online
+    }
 }
+
 
 Clear-Host
 $Logo
