f866a36e2ed6248e50a8aa19846ece860de683a3
Jake Hildreth <jakehildreth@trimarcsecurity.com>
2023-06-24 07:54:38 -0400

    Added "Any Purpose" EKU to Find-ESC2

--- Invoke-Locksmith.ps1
+++ Invoke-Locksmith.ps1
@@ -443,7 +443,7 @@ function Find-ESC2 {
     )
     $ADCSObjects | Where-Object {
         ($_.ObjectClass -eq 'pKICertificateTemplate') -and
-        (!$_.pkiExtendedKeyUsage) -and 
+        ( (!$_.pkiExtendedKeyUsage) -or ($_.pkiExtendedKeyUsage -match '2.5.29.37.0') )-and 
         ($_.'msPKI-Certificate-Name-Flag' -eq 1) -and
         ($_.'msPKI-Enrollment-Flag' -ne 2) -and
         ( ($_.'msPKI-RA-Signature' -eq 0) -or ($null -eq $_.'msPKI-RA-Signature') ) 
