ef8b5a53b652be7df069e2e1d00d3dd975530c3a
Jake Hildreth <jakehildreth@trimarcsecurity.com>
2023-06-28 18:36:51 -0400

    Beginning ESC3 coverage

--- Invoke-Locksmith.ps1
+++ Invoke-Locksmith.ps1
@@ -77,6 +77,7 @@ $AllDomainsCertPublishersSIDs = @()
 $AllDomainsDomainAdminSIDs = @()
 $ClientAuthEKUs = '1\.3\.6\.1\.5\.5\.7\.3\.2|1\.3\.6\.1\.5\.2\.3\.4|1\.3\.6\.1\.4\.1\.311\.20\.2\.2|2\.5\.29\.37\.0'
 $DangerousRights = 'GenericAll|WriteDacl|WriteOwner|WriteProperty'
+$EnrollmentAgentEKU = '1\.3\.6\.1\.4\.1\.311\.20\.2\.1'
 $ForestGC = $(Get-ADDomainController -Discover -Service GlobalCatalog -ForceDiscover | Select-Object -ExpandProperty Hostname) + ":3268"
 $SafeOwners = '-512$|-519$|-544$|-18$|-517$|-500$'
 $SafeUsers = '-512$|-519$|-544$|-18$|-517$|-500$|-516$|-9$'
@@ -469,6 +470,49 @@ function Find-ESC2 {
                 $Issue | Add-Member -MemberType NoteProperty -Name Revert `
                     -Value "Get-ADObject `'$($_.DistinguishedName)`' | Set-ADObject -Replace @{'msPKI-Certificate-Name-Flag' = 1}"  -Force
                 $Issue | Add-Member -MemberType NoteProperty -Name Technique -Value 'ESC2'
+                $Issue
+            }
+        }
+    }
+}
+
+
+function Find-ESC3Condition1 {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)]
+        [array]$ADCSObjects,
+        [Parameter(Mandatory = $true)]
+        [array]$SafeUsers
+    )
+    $ADCSObjects | Where-Object {
+        ($_.objectClass -eq 'pKICertificateTemplate') -and
+        ($_.pkiExtendedKeyUsage -match $EnrollmentAgentEKU) -and
+        ($_.'msPKI-Certificate-Name-Flag' -eq 1) -and
+        ($_.'msPKI-Enrollment-Flag' -ne 2) -and
+        ( ($_.'msPKI-RA-Signature' -eq 0) -or ($null -eq $_.'msPKI-RA-Signature') )
+    } | ForEach-Object {
+        foreach ($entry in $_.nTSecurityDescriptor.Access) {
+            $Principal = New-Object System.Security.Principal.NTAccount($entry.IdentityReference)
+            if ($Principal -match '^(S-1|O:)') {
+                $SID = $Principal
+            } else {
+                $SID = ($Principal.Translate([System.Security.Principal.SecurityIdentifier])).Value
+            }
+            if ( ($SID -notmatch $SafeUsers) -and ($entry.ActiveDirectoryRights -match 'ExtendedRight') ) {
+                $Issue = New-Object -TypeName pscustomobject
+                $Issue | Add-Member -MemberType NoteProperty -Name Forest -Value $_.CanonicalName.split('/')[0] -Force
+                $Issue | Add-Member -MemberType NoteProperty -Name Name -Value $_.Name -Force
+                $Issue | Add-Member -MemberType NoteProperty -Name DistinguishedName -Value $_.DistinguishedName -Force
+                $Issue | Add-Member -MemberType NoteProperty -Name IdentityReference -Value $entry.IdentityReference -Force
+                $Issue | Add-Member -MemberType NoteProperty -Name ActiveDirectoryRights -Value $entry.ActiveDirectoryRights -Force
+                $Issue | Add-Member -MemberType NoteProperty -Name Issue `
+                    -Value "$($entry.IdentityReference) can enroll in this Client Authentication template using a SAN without Manager Approval"  -Force
+                $Issue | Add-Member -MemberType NoteProperty -Name Fix `
+                    -Value "Get-ADObject `'$($_.DistinguishedName)`' | Set-ADObject -Replace @{'msPKI-Certificate-Name-Flag' = 0}" -Force
+                $Issue | Add-Member -MemberType NoteProperty -Name Revert `
+                    -Value "Get-ADObject `'$($_.DistinguishedName)`' | Set-ADObject -Replace @{'msPKI-Certificate-Name-Flag' = 1}"  -Force
+                $Issue | Add-Member -MemberType NoteProperty -Name Technique -Value 'ESC1'
                 $Issue
             }
         }
