68ab59a78622b1cbcc893d05d847b4daa2374066
Jake Hildreth <jakehildreth@trimarcsecurity.com>
2023-07-29 17:31:12 -0400

    ESC1 and ESC2 Positive Tests complete.

--- Tests/Invoke-TSS.ps1
+++ Tests/Invoke-TSS.ps1
@@ -43,6 +43,23 @@ $NewObjects = @(
     'ESC5WriteOwner'
 )
 
+$Administrators = New-Object System.Security.Principal.SecurityIdentifier('S-1-5-32-544')
+$AuthenticatedUsers = New-Object System.Security.Principal.SecurityIdentifier('S-1-5-11')
+
+$ExtendedRight = [System.DirectoryServices.ActiveDirectoryRights]::ExtendedRight
+$GenericAll = [System.DirectoryServices.ActiveDirectoryRights]::GenericAll
+$GenericRead = [System.DirectoryServices.ActiveDirectoryRights]::GenericRead
+$ReadProperty = [System.DirectoryServices.ActiveDirectoryRights]::ReadProperty
+$WriteOwner = [System.DirectoryServices.ActiveDirectoryRights]::WriteOwner
+$WriteProperty = [System.DirectoryServices.ActiveDirectoryRights]::WriteProperty
+
+$DefaultRights = $ExtendedRight + $GenericRead + $ReadProperty + $WriteProperty
+
+$EnrollGUID = [GUID]'0e10c968-78fb-11d2-90d4-00c04f79dc55'
+$AutoEnrollGUID = [GUID]'a05b8cc2-17bc-4802-a710-e7c15ab866a2'
+
+$Allow = [System.Security.AccessControl.AccessControlType]::Allow
+
 $PKSContainer = "CN=Public Key Services,CN=Services,CN=Configuration,$((Get-ADRootDSE).defaultNamingContext)"
 
 $NewTemplates | ForEach-Object {
@@ -52,3 +69,40 @@ $NewTemplates | ForEach-Object {
 $NewObjects | ForEach-Object {
     New-ADObject -Name $_ -Type 'container' -Path $PKSContainer
 }
+
+$ESC1 = Get-ADObject "CN=ESC1,CN=Certificate Templates,$PKSContainer" -Properties *
+$ESC1Properties = @{
+    'msPKI-Certificate-Name-Flag' = 1
+    'msPKI-Enrollment-Flag' = 0
+    'pKIExtendedKeyUsage' = '1.3.6.1.5.5.7.3.2'
+} 
+Set-ADObject $ESC1.DistinguishedName -Add $ESC1Properties
+$ACL = Get-Acl "AD:$ESC1"
+$AccessRule = New-Object System.DirectoryServices.ActiveDirectoryAccessRule $AuthenticatedUsers,$DefaultRights,$Allow,$EnrollGUID
+$ACL.AddAccessRule($AccessRule)
+Set-Acl "AD:$ESC1" -AclObject $ACL
+
+$ESC1and2 = Get-ADObject "CN=ESC1and2,CN=Certificate Templates,$PKSContainer" -Properties *
+$ESC1and2Properties = @{
+    'msPKI-Certificate-Name-Flag' = 1
+    'msPKI-Enrollment-Flag' = 0
+    'pKIExtendedKeyUsage' = '2.5.29.37.0'
+} 
+Set-ADObject $ESC1and2.DistinguishedName -Add $ESC1and2Properties
+$ACL = Get-Acl "AD:$ESC1and2"
+$AccessRule = New-Object System.DirectoryServices.ActiveDirectoryAccessRule $AuthenticatedUsers,$DefaultRights,$Allow,$EnrollGUID
+$ACL.AddAccessRule($AccessRule)
+Set-Acl "AD:$ESC1and2" -AclObject $ACL
+
+$ESC2 = Get-ADObject "CN=ESC2,CN=Certificate Templates,$PKSContainer" -Properties *
+$ESC2Properties = @{
+    'msPKI-Certificate-Name-Flag' = 1
+    'msPKI-Enrollment-Flag' = 0
+    'pKIExtendedKeyUsage' = '2.5.29.37.0'
+}   
+Set-ADObject $ESC2.DistinguishedName -Add $ESC2Properties
+Set-ADObject $ESC2.DistinguishedName -Clear pKIExtendedKeyUsage
+$ACL = Get-Acl "AD:$ESC2"
+$AccessRule = New-Object System.DirectoryServices.ActiveDirectoryAccessRule $AuthenticatedUsers,$DefaultRights,$Allow,$EnrollGUID
+$ACL.AddAccessRule($AccessRule)
+Set-Acl "AD:$ESC2" -AclObject $ACL
