93c2c4c5454c73051892d23a154fcbca001b8cec
Jake Hildreth <jakehildreth@trimarcsecurity.com>
2023-08-19 11:08:10 -0400

    Small tools to help with comparing AD Objects

---
+++ Tests/Show-ADObjectChanges.ps1
@@ -0,0 +1,31 @@
+# Source: https://learn.microsoft.com/en-us/archive/blogs/janesays/compare-all-properties-of-two-objects-in-windows-powershell
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$DN
+)
+
+$ReferenceObject = Get-ADObject -Identity $DN -Properties *
+Read-Host "Make your changes and press Enter"
+$DifferenceObject = Get-ADObject -Identity $DN -Properties *
+
+$ObjectProperties = $ReferenceObject | Get-Member -MemberType Property,NoteProperty | % Name
+$ObjectProperties += $DifferenceObject | Get-Member -MemberType Property,NoteProperty | % Name
+$ObjectProperties = $ObjectProperties | Sort | Select -Unique
+$Differences = @()
+
+foreach ($objectproperty in $ObjectProperties) {
+    $difference = Compare-Object $ReferenceObject $DifferenceObject -Property $objectproperty
+    if ($difference) {
+        $differenceproperties = @{
+            PropertyName = $objectproperty
+            RefValue = ($difference | ? {$_.SideIndicator -eq '<='} | % $($objectproperty))
+            DiffValue = ($difference | ? {$_.SideIndicator -eq '=>'} | % $($objectproperty))
+        }
+        $Differences += New-Object PSObject -Property $differenceproperties
+    }
+}
+if ($Differences) {
+    return (
+        $Differences | Select PropertyName,RefValue,DiffValue
+    )
+}
