97dc81f9780cff0672f49b2fb5bd9699e3110b1f
Sam Erde <samuel.erde@gmail.com>
2023-08-29 16:34:54 -0400

    Replace find-issue scripts with Invoke-Scans

--- Public/Invoke-Locksmith.ps1
+++ Public/Invoke-Locksmith.ps1
@@ -82,9 +82,9 @@
         [string]$InputPath,
         [string]$Mode = 0,
         [System.Management.Automation.PSCredential]$Credential,
-        [Parameter(HelpMessage = 'The default action is `"Scan`" to find all misconfigurations. `"Fix`" will prompt you before fixing each individual finding.')]
-            [ValidateSet('Scan','Fix', ErrorMessage = 'Please choose Scan or Fix for your action.')]
-            [string]$Action = 'Scan',
+        [Parameter()]
+            [ValidateSet('Auditing','ESC1','ESC2','ESC3','ESC4','ESC5','ESC6','ESC8','All','PromptMe')]
+            [array]$Scans = 'All',
         [Parameter()]
             [ValidateSet('Findings','Detailed', ErrorMessage = 'Please choose Findings or Detailed for your output. Detailed output includes the fixes.')]
             [string]$Output = 'Findings',
@@ -190,28 +190,7 @@
         $CAHosts | ForEach-Object { $SafeUsers += '|' + $_.Name }
     }
 
-    Write-Host 'Identifying auditing issues...'
-    [array]$AuditingIssues = Find-AuditingIssue -ADCSObjects $ADCSObjects
-
-    Write-Host 'Identifying AD CS templates with dangerous configurations...'
-    [array]$ESC1 = Find-ESC1 -ADCSObjects $ADCSObjects -SafeUsers $SafeUsers
-    [array]$ESC2 = Find-ESC2 -ADCSObjects $ADCSObjects -SafeUsers $SafeUsers
-
-    Write-Host 'Identifying AD CS template and other objects with poor access control...'
-    [array]$ESC4 = Find-ESC4 -ADCSObjects $ADCSObjects -SafeUsers $SafeUsers -DangerousRights $DangerousRights -SafeOwners $SafeOwners
-    [array]$ESC5 = Find-ESC5 -ADCSObjects $ADCSObjects -SafeUsers $SafeUsers -DangerousRights $DangerousRights -SafeOwners $SafeOwners
-    [array]$ESC6 = Find-ESC6 -ADCSObjects $ADCSObjects
-
-    Write-Host 'Identifying HTTP-based certificate enrollment interfaces...'
-    [array]$ESC8 = Find-ESC8 -ADCSObjects $ADCSObjects
-
-    [array]$AllIssues = $AuditingIssues + $ESC1 + $ESC2 + $ESC4 + $ESC5 + $ESC6 + $ESC8
-
-    # If these are all empty = no issues found, exit
-    if ((!$AuditingIssues) -and (!$ESC1) -and (!$ESC2) -and (!$ESC4) -and (!$ESC5) -and (!$ESC6) -and (!$ESC8) ) {
-        Write-Host "`n$(Get-Date) : No ADCS issues were found." -ForegroundColor Green
-        break
-    }
+    Invoke-Scans -Scans $Scans
 
     switch ($Mode) {
         0 {
