4655443d48f6cbc28e9776cda07c07da4dc0e6b5
Sam Erde <samuel.erde@gmail.com>
2023-09-29 06:53:12 -0400

    Changed FindIt and FixIt to scriptblocks

--- Private/Create-Dictionary.ps1
+++ Private/Create-Dictionary.ps1
@@ -12,8 +12,8 @@ class VulnerableConfig {
     [string]$Name
     [string]$Description
     [string]$Category
-    [string]$FindIt
-    [string]$FixIt
+    [scriptblock]$FindIt
+    [scriptblock]$FixIt
     [uri]$ReferenceUrl
 }
 
@@ -24,71 +24,71 @@ $Dictionary = @(
         Description = 'Misconfigured Certificate Templates'
         Category = "Escalation Path"
         FindIt =  {[array]$AuditingIssues = Find-AuditingIssue -ADCSObjects $ADCSObjects}
-        FixIt = Write-Output "Add code to fix the vulnerable configuration."
+        FixIt = {Write-Output "Add code to fix the vulnerable configuration."}
         ReferenceUrl = 'https://posts.specterops.io/certified-pre-owned-d95910965cd2#:~:text=Misconfigured%20Certificate%20Templates%20%E2%80%94%20ESC1'
     },
     [VulnerableConfig]@{
         Name = 'ESC2';
         Description = 'Misconfigured Certificate Templates'
         Category = "Escalation Path"
-        FindIt =  'Find-ESC2'
-        FixIt = Write-Output 'Add code to fix the vulnerable configuration.'
+        FindIt =  {Find-ESC2}
+        FixIt = {Write-Output 'Add code to fix the vulnerable configuration.'}
         ReferenceUrl = 'https://posts.specterops.io/certified-pre-owned-d95910965cd2#:~:text=Misconfigured%20Certificate%20Templates%20%E2%80%94%20ESC2'
     },
     [VulnerableConfig]@{
         Name = 'ESC3'
         Description = 'Enrollment Agent Templates'
         Category = "Escalation Path"
-        FindIt =  'Find-ESC3'
-        FixIt = Write-Output 'Add code to fix the vulnerable configuration.'
+        FindIt =  {Find-ESC3}
+        FixIt = {Write-Output 'Add code to fix the vulnerable configuration.'}
         ReferenceUrl = 'https://posts.specterops.io/certified-pre-owned-d95910965cd2#:~:text=Enrollment%20Agent%20Templates%20%E2%80%94%20ESC3'
     },
     [VulnerableConfig]@{ 
         Name = 'ESC4';
         Description = 'Vulnerable Certificate Template Access Control'
         Category = "Escalation Path"
-        FindIt =  'Find-ESC4'
-        FixIt = Write-Output 'Add code to fix the vulnerable configuration.'
+        FindIt =  {Find-ESC4}
+        FixIt = {Write-Output 'Add code to fix the vulnerable configuration.'}
         ReferenceUrl = 'https://posts.specterops.io/certified-pre-owned-d95910965cd2#:~:text=Vulnerable%20Certificate%20Template%20Access%20Control%20%E2%80%94%20ESC4'
     },
     [VulnerableConfig]@{
         Name = 'ESC5';
         Description = 'Vulnerable PKI Object Access Control'
         Category = "Escalation Path"
-        FindIt =  'Find-ESC5'
-        FixIt = Write-Output 'Add code to fix the vulnerable configuration.'
+        FindIt =  {Find-ESC5}
+        FixIt = {Write-Output 'Add code to fix the vulnerable configuration.'}
         ReferenceUrl = 'https://posts.specterops.io/certified-pre-owned-d95910965cd2#:~:text=Vulnerable%20PKI%20Object%20Access%20Control%20%E2%80%94%20ESC5'
     },
     [VulnerableConfig]@{
         Name = 'ESC6'
         Description = 'EDITF_ATTRIBUTESUBJECTALTNAME2'
         Category = "Escalation Path"
-        FindIt =  'Find-ESC6'
-        FixIt = Write-Output 'Add code to fix the vulnerable configuration.'
+        FindIt =  {Find-ESC6}
+        FixIt = {Write-Output 'Add code to fix the vulnerable configuration.'}
         ReferenceUrl = 'https://posts.specterops.io/certified-pre-owned-d95910965cd2#:~:text=EDITF_ATTRIBUTESUBJECTALTNAME2%20%E2%80%94%20ESC6'
     },
     [VulnerableConfig]@{
         Name = 'ESC7'
         Description = 'Vulnerable Certificate Authority Access Control'
         Category = "Escalation Path"
-        FindIt =  'Find-ESC7'
-        FixIt = Write-Output 'Add code to fix the vulnerable configuration.'
+        FindIt =  {Find-ESC7}
+        FixIt = {Write-Output 'Add code to fix the vulnerable configuration.'}
         ReferenceUrl = 'https://posts.specterops.io/certified-pre-owned-d95910965cd2#:~:text=Vulnerable%20Certificate%20Authority%20Access%20Control%20%E2%80%94%20ESC7'
     },
     [VulnerableConfig]@{
         Name = 'ESC8'
         Description = 'NTLM Relay to AD CS HTTP Endpoints'
         Category = "Escalation Path"
-        FindIt =  'Find-ESC8'
-        FixIt = Write-Output 'Add code to fix the vulnerable configuration.'
+        FindIt =  {Find-ESC8}
+        FixIt = {Write-Output 'Add code to fix the vulnerable configuration.'}
         ReferenceUrl = 'https://posts.specterops.io/certified-pre-owned-d95910965cd2#:~:text=NTLM%20Relay%20to%20AD%20CS%20HTTP%20Endpoints'
     },
     [VulnerableConfig]@{
         Name = 'Auditing'
         Description = 'Gaps in auditing on certificate authorities and AD CS objects.'
         Category = "Server Configuration"
-        FindIt =  'Find-AuditingIssue'
-        FixIt = Write-Output 'Add code to fix the vulnerable configuration.'
+        FindIt =  {Find-AuditingIssue}
+        FixIt = {Write-Output 'Add code to fix the vulnerable configuration.'}
         ReferenceUrl = ''
     }
 )
