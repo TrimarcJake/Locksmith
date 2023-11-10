f526c3282afb2137f151cdf3b42133ed6bc75dec
Sam Erde <20478745+SamErde@users.noreply.github.com>
2023-11-10 13:22:38 -0500

    Short "no findings" check

--- Public/Invoke-Locksmith.ps1
+++ Public/Invoke-Locksmith.ps1
@@ -201,30 +201,8 @@
             $ESC8           = $Results['ESC8']
     }
 
-    if ( -not ($Scans) -and (-not ($Results)) ) {
-        Write-Host 'Identifying auditing issues...'
-        [array]$AuditingIssues = Find-AuditingIssue -ADCSObjects $ADCSObjects
-        Write-Host 'Identifying AD CS templates with dangerous ESC1 configurations...'
-        [array]$ESC1 = Find-ESC1 -ADCSObjects $ADCSObjects -SafeUsers $SafeUsers
-        Write-Host 'Identifying AD CS templates with dangerous ESC2 configurations...'
-        [array]$ESC2 = Find-ESC2 -ADCSObjects $ADCSObjects -SafeUsers $SafeUsers
-        Write-Host 'Identifying AD CS templates with dangerous ESC3 configurations...'
-        [array]$ESC3 = Find-ESC3Condition1 -ADCSObjects $ADCSObjects -SafeUsers $SafeUsers
-        [array]$ESC3 += Find-ESC3Condition2 -ADCSObjects $ADCSObjects -SafeUsers $SafeUsers
-        Write-Host 'Identifying AD CS template and other objects with poor access control (ESC4)...'
-        [array]$ESC4 = Find-ESC4 -ADCSObjects $ADCSObjects -SafeUsers $SafeUsers -DangerousRights $DangerousRights -SafeOwners $SafeOwners
-        Write-Host 'Identifying AD CS template and other objects with poor access control (ESC5)...'
-        [array]$ESC5 = Find-ESC5 -ADCSObjects $ADCSObjects -SafeUsers $SafeUsers -DangerousRights $DangerousRights -SafeOwners $SafeOwners
-        Write-Host 'Identifying AD CS template and other objects with poor access control (ESC6)...'
-        [array]$ESC6 = Find-ESC6 -ADCSObjects $ADCSObjects
-        Write-Host 'Identifying HTTP-based certificate enrollment interfaces (ESC8)...'
-        [array]$ESC8 = Find-ESC8 -ADCSObjects $ADCSObjects
-
-        [array]$AllIssues = $AuditingIssues + $ESC1 + $ESC2 + $ESC3 + $ESC4 + $ESC5 + $ESC6 + $ESC8
-    }
-
     # If these are all empty = no issues found, exit
-    if ((!$AuditingIssues) -and (!$ESC1) -and (!$ESC2) -and (!$ESC3) -and (!$ESC4) -and (!$ESC5) -and (!$ESC6) -and (!$ESC8) ) {
+    if ($null -eq $Results) {
         Write-Host "`n$(Get-Date) : No ADCS issues were found." -ForegroundColor Green
         break
     }
