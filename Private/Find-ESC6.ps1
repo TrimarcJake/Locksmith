adf784fd52d8c1f5deb0eb39c25d1cc44ec8153a
Jake Hildreth <jakehildreth@trimarcsecurity.com>
2024-01-14 09:33:36 -0500

    Ope, overselected on that last find/replace.

--- Private/Find-ESC6.ps1
+++ Private/Find-ESC6.ps1
@@ -11,22 +11,20 @@
         } | ForEach-Object {
             [string]$CAFullName = "$($_.dNSHostName)\$($_.Name)"
             $Issue = New-Object -TypeName pscustomobject
-            $Issue | Add-Member -MemberType NoteProperty -Name Forest = $_.CanonicalName.split('/')[0] -Force
-            $Issue | Add-Member -MemberType NoteProperty -Name Name = $_.Name -Force
-            $Issue | Add-Member -MemberType NoteProperty -Name DistinguishedName = $_.DistinguishedName -Force
+            $Issue | Add-Member -MemberType NoteProperty -Name Forest -Value $_.CanonicalName.split('/')[0] -Force
+            $Issue | Add-Member -MemberType NoteProperty -Name Name -Value $_.Name -Force
+            $Issue | Add-Member -MemberType NoteProperty -Name DistinguishedName -Value $_.DistinguishedName -Force
             if ($_.SANFlag -eq 'Yes') {
-                $Issue | Add-Member -MemberType NoteProperty -Name Issue = 'EDITF_ATTRIBUTESUBJECTALTNAME2 is enabled.' -Force
-                $Issue | Add-Member -MemberType NoteProperty -Name Fix = "certutil -config $CAFullname -setreg policy\EditFlags -EDITF_ATTRIBUTESUBJECTALTNAME2; Invoke-Command -ComputerName `"$($_.dNSHostName)`" -ScriptBlock { Get-Service -Name `'certsvc`' | Restart-Service -Force }" -Force
-                $Issue | Add-Member -MemberType NoteProperty -Name Revert = "certutil -config $CAFullname -setreg policy\EditFlags +EDITF_ATTRIBUTESUBJECTALTNAME2; Invoke-Command -ComputerName `"$($_.dNSHostName)`" -ScriptBlock { Get-Service -Name `'certsvc`' | Restart-Service -Force }" -Force
+                $Issue | Add-Member -MemberType NoteProperty -Name Issue -Value 'EDITF_ATTRIBUTESUBJECTALTNAME2 is enabled.' -Force
+                $Issue | Add-Member -MemberType NoteProperty -Name Fix -Value "certutil -config $CAFullname -setreg policy\EditFlags -EDITF_ATTRIBUTESUBJECTALTNAME2; Invoke-Command -ComputerName `"$($_.dNSHostName)`" -ScriptBlock { Get-Service -Name `'certsvc`' | Restart-Service -Force }" -Force
+                $Issue | Add-Member -MemberType NoteProperty -Name Revert -Value "certutil -config $CAFullname -setreg policy\EditFlags +EDITF_ATTRIBUTESUBJECTALTNAME2; Invoke-Command -ComputerName `"$($_.dNSHostName)`" -ScriptBlock { Get-Service -Name `'certsvc`' | Restart-Service -Force }" -Force
             }
             else {
-                $Issue | Add-Member -MemberType NoteProperty -Name Issue = $_.AuditFilter -Force
-                $Issue | Add-Member -MemberType NoteProperty -Name Fix = 'N/A' -Force
-                $Issue | Add-Member -MemberType NoteProperty -Name Revert = 'N/A' -Force
+                $Issue | Add-Member -MemberType NoteProperty -Name Issue -Value $_.AuditFilter -Force
+                $Issue | Add-Member -MemberType NoteProperty -Name Fix -Value 'N/A' -Force
+                $Issue | Add-Member -MemberType NoteProperty -Name Revert -Value 'N/A' -Force
             }
-            $Issue | Add-Member -MemberType NoteProperty -Name Technique = 'ESC6'
-            $Severity = Set-Severity -Issue $Issue
-            $Issue | Add-Member -MemberType NoteProperty -Name Severity = $Severity
+            $Issue | Add-Member -MemberType NoteProperty -Name Technique -Value 'ESC6'
             $Issue
         }
     }
