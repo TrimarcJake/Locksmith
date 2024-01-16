e2ed7c171d532793022e2121c68c3594d4c57f64
Jake Hildreth <jakehildreth@trimarcsecurity.com>
2024-01-15 19:44:38 -0500

    Cleaned up logic a bit.

--- Private/Find-ESC6.ps1
+++ Private/Find-ESC6.ps1
@@ -15,18 +15,15 @@
                 Name              = $_.Name
                 DistinguishedName = $_.DistinguishedName
                 Technique         = 'ESC6'
+                Issue             = $_.AuditFilter
+                Fix               = 'N/A'
+                Revert            = 'N/A'
             }
             if ($_.SANFlag -eq 'Yes') {
                 $Issue['Issue']  = 'EDITF_ATTRIBUTESUBJECTALTNAME2 is enabled.'
                 $Issue['Fix']    = "certutil -config $CAFullname -setreg policy\EditFlags -EDITF_ATTRIBUTESUBJECTALTNAME2; Invoke-Command -ComputerName `"$($_.dNSHostName)`" -ScriptBlock { Get-Service -Name `'certsvc`' | Restart-Service -Force }"
                 $Issue['Revert'] = "certutil -config $CAFullname -setreg policy\EditFlags +EDITF_ATTRIBUTESUBJECTALTNAME2; Invoke-Command -ComputerName `"$($_.dNSHostName)`" -ScriptBlock { Get-Service -Name `'certsvc`' | Restart-Service -Force }"
             }
-            else {
-                $Issue['Issue']  = $_.AuditFilter
-                $Issue['Fix']    = 'N/A'
-                $Issue['Revert'] = 'N/A'
-            }
-
             $Issue
         }
     }
