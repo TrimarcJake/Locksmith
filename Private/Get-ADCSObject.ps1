707f754698838b064e28b445c839d278c4ff5073
Sam Erde <20478745+SamErde@users.noreply.github.com>
2024-02-26 06:43:25 -0500

    Update Get-ADCSObject.ps1
    Made indentation consistent with other CBH blocks.

--- Private/Get-ADCSObject.ps1
+++ Private/Get-ADCSObject.ps1
@@ -1,21 +1,21 @@
 ﻿function Get-ADCSObject {
     <#
     .SYNOPSIS
-    Retrieves Active Directory Certificate Services (AD CS) objects.
+        Retrieves Active Directory Certificate Services (AD CS) objects.
 
     .DESCRIPTION
-    This script retrieves AD CS objects from the specified forests.
-    It can be used to gather information about Public Key Services in Active Directory.
+        This script retrieves AD CS objects from the specified forests.
+        It can be used to gather information about Public Key Services in Active Directory.
 
     .PARAMETER Targets
-    Specifies the forest(s) from which to retrieve AD CS objects.
+        Specifies the forest(s) from which to retrieve AD CS objects.
 
     .PARAMETER Credential
-    Specifies the credentials to use for authentication when retrieving ADCS objects.
+        Specifies the credentials to use for authentication when retrieving ADCS objects.
 
     .EXAMPLE
-    Get-ADCSObject -Targets forest1.lan -Credential $cred
-    This example retrieves ADCS objects from forest1.lan using the specified credentials.
+        Get-ADCSObject -Targets forest1.lan -Credential $cred
+        This example retrieves ADCS objects from forest1.lan using the specified credentials.
 
     #>
 
