7782a1ceab6132c3b9dc6ab0fdf365434b540e05
TrimarcJake <jakehildreth@trimarcsecurity.com>
2024-07-20 06:44:57 -0500

    #126 Initial commit

---
+++ Private/Update-ESC4Remediation.ps1
@@ -0,0 +1,38 @@
+function Update-ESC4Remediation.ps1 {
+    <#
+    .SYNOPSIS
+        This function asks the user a set of questions to provide the most appropriate remediation for ESC4 issues.
+
+    .DESCRIPTION
+
+
+    .PARAMETER Issue
+
+
+    .PARAMETER Mode
+
+
+    .OUTPUTS
+        This function updates ESC4 remediations customized to the user's needs.
+
+    .EXAMPLE
+        $Target = Get-Target
+        $ADCSObjects = Get-ADCSObject -Target $Target
+        $DangerousRights = @('GenericAll', 'WriteProperty', 'WriteOwner', 'WriteDacl')
+        $SafeOwners = '-512$|-519$|-544$|-18$|-517$|-500$'
+        $SafeUsers = '-512$|-519$|-544$|-18$|-517$|-500$|-516$|-9$|-526$|-527$|S-1-5-10'
+        $SafeObjectTypes = '0e10c968-78fb-11d2-90d4-00c04f79dc55|a05b8cc2-17bc-4802-a710-e7c15ab866a2'
+        $ESC4Issues = Find-ESC4 -ADCSObjects $ADCSObjects -DangerousRights $DangerousRights -SafeOwners $SafeOwners -SafeUsers $SafeUsers -SafeObjectTypes $SafeObjectTypes
+        Update-ESC4Remediation -ESC4Issues $ESC4Issues
+    #>
+    [CmdletBinding()]
+    param(
+        $ESC4Issues,
+        [Parameter(Mandatory = $true)]
+        [int]$Mode
+    )
+
+    $ESC4Issues | ForEach-Object -PipelineVariable Issue {
+        $_
+    }
+}
