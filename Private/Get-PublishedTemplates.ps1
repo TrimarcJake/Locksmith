09568ff2abbb8f8233e3c735a9fc0bf96030ef7a
Sam Erde <20478745+SamErde@users.noreply.github.com>
2024-08-30 14:46:51 -0400

    Create Get-PublishedTemplates.ps1
    Get-PublishedTemplates pulls a list of published templates with their name, OID, flags, enrollment flag, last modified date, revision, and minor revision. This should help us check if vulnerable templates are published and also ultimately check if the vulnerable version is in use by issued certificate requests.

---
+++ Private/Get-PublishedTemplates.ps1
@@ -0,0 +1,62 @@
+function Get-PublishedTemplates {
+    <#
+    .SYNOPSIS
+    Get published certificate templates from Active Directory.
+
+    .DESCRIPTION
+    Gets all templates from Active Directory and identifies which ones are published.
+
+    .EXAMPLE
+    Get-PublishedTemplates
+
+    .NOTES
+        If either of these flags are set, the template is considered published:
+
+        - CT_FLAG_IS_CA      (0x1): This bit indicates whether the template is for a Certification Authority (CA). If this bit is set, the template is considered published.
+        - CT_FLAG_IS_DEFAULT (0x2): This bit indicates whether the template is a default template. If this bit is set, the template is also considered published.
+
+        If pkiEnrollmentFlag has 0x10 (CT_FLAG_PUBLISH_TO_DS) set, the certificate is published to Active Directory.
+    #>
+    [CmdletBinding()]
+    param ()
+
+    $ADSISearcher = [adsisearcher]'(objectClass=*)'
+    $ADSISearcher.SearchRoot = [adsi]'LDAP://RootDSE'
+    $ConfigurationNamingContext = $ADSISearcher.SearchRoot.Properties['configurationNamingContext'][0]
+
+    # Set the [adsisearcher] filter, search root, and other options
+    $ADSISearcher = [adsisearcher]'(objectClass=pKICertificateTemplate)'
+    $ADSISearcher.SearchRoot = [ADSI]"LDAP://CN=Certificate Templates,CN=Public Key Services,CN=Services,$ConfigurationNamingContext"
+
+    $Results = $ADSISearcher.FindAll()
+
+    [array]$Templates = @()
+
+    foreach ($item in $Results) {
+        $Template = $item.GetDirectoryEntry()
+        $TemplateName = $Template.Properties['Name'][0]
+        $OID = $item.Properties['mspki-cert-template-oid'][0]
+        $Flags = $Template.Properties['flags'][0]
+        $EnrollmentFlag = $item.Properties['mspki-enrollment-flag'][0]
+        $LastModified = $item.Properties['whenchanged'][0]
+        $Revision = $item.Properties['Revision'][0]
+        $MinorRevision = $item.Properties['mspki-template-minor-revision'][0]
+
+        # Check if the template is published
+        $IsPublished = (($Flags -band 0x1) -ne 0 -or ($Flags -band 0x2) -ne 0) -or ($EnrollmentFlag -band 0x10)
+
+        $Templates += (
+            [PSCustomObject]@{
+                Name = $TemplateName
+                OID = $OID
+                Flags = $Flags
+                EnrollmentFlag = $EnrollmentFlag
+                IsPublished = $IsPublished
+                LastModified = $LastModified
+                Revision = $Revision
+                MinorRevision = $MinorRevision
+            }
+        )
+    }
+    return $Templates | Where-Object {$_.IsPublished -eq $true}
+}
